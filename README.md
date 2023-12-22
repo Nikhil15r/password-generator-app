0ef0a97c75dfa768c114227f8d4d354fd69b0029
Nikhil14r <nikhiljaiswal084@gmail.com>
2023-12-22 13:06:17 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,62 @@
+# React Password Generator
+
+A simple React application for generating secure passwords with customizable options.
+
+## Table of Contents
+
+- [Features](#features)
+- [Demo](#demo)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Features
+
+- Generate secure passwords with customizable length.
+- Include or exclude numbers and special characters in generated passwords.
+- Copy generated passwords to the clipboard with a single click.
+
+## Demo
+
+[Live Demo](#) 
+
+## Installation
+
+1. Clone the repository:
+
+   ```bash
+   git clone https://github.com/Nikhil15r/react-password-generator.git
+   ```
+
+2. Navigate to the project directory:
+
+   ```bash
+   cd react-password-generator
+   ```
+
+3. Install dependencies:
+
+   ```bash
+   npm install
+   ```
+
+## Usage
+
+1. Start the development server:
+
+   ```bash
+   npm start
+   ```
+
+2. Open your browser and visit [http://localhost:5173](http://localhost:5173).
+
+3. Use the password generator to customize and generate secure passwords.
+
+## Contributing
+
+Contributions are welcome! If you find a bug or have a suggestion for improvement, please open an issue or submit a pull request.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
---
+++ src/App.jsx
@@ -0,0 +1,115 @@
+import React, { useState, useCallback, useEffect, useRef } from 'react';
+import './App.css';
+
+function App() {
+  const [state, setState] = useState({
+    password: '',
+    length: 8,
+    numberAllowed: false,
+    charAllowed: false,
+    refresh: 0, // Step 1: Add a refresh token state
+  });
+
+  const passwordRef = useRef(null);
+
+  const passwordGenerator = useCallback(() => {
+    let pass = '';
+    let str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+
+    if (state.numberAllowed) str += '0123456789';
+    if (state.charAllowed) str += '!@#$%^&*_-+=[]{}/~';
+
+    for (let i = 1; i <= state.length; i++) {
+      let char = Math.floor(Math.random() * str.length);
+      pass += str.charAt(char);
+    }
+
+    setState((prev) => ({ ...prev, password: pass }));
+  }, [state.length, state.numberAllowed, state.charAllowed, state.refresh]);
+
+  const copyPasswordToClipboard = useCallback(() => {
+    try {
+      passwordRef.current?.select();
+      passwordRef.current?.setSelectionRange(0, 100);
+      window.navigator.clipboard.writeText(state.password);
+    } catch (err) {
+      console.error('Error copying to clipboard:', err);
+    }
+  }, [state.password]);
+
+  const handleRefreshPassword = () => {
+    // Step 2: Increment the refresh token to trigger password regeneration
+    setState((prev) => ({ ...prev, refresh: prev.refresh + 1 }));
+  };
+
+  useEffect(() => {
+    passwordGenerator();
+  }, [state.length, state.numberAllowed, state.charAllowed, state.refresh, passwordGenerator]);
+
+  return (
+    <>
+      {/* <div className='flex flex-col items-center justify-center h-screen'> */}
+      <div className='w-full max-w-md mx-auto my-8 py-3 px-4 shadow-md rounded-lg text-orange-500 bg-gray-800'>
+        <h1 className='text-white text-center my-3'>Password Generator</h1>
+        <div className='flex shadow rounded-lg overflow-hidden mb-4'>
+          <input
+            type='text'
+            value={state.password}
+            className='outline-none w-full py-1 px-3'
+            placeholder='Password'
+            ref={passwordRef}
+            readOnly
+          />
+          <button
+            onClick={copyPasswordToClipboard}
+            className='copy-btn ouline-none text-white px-3 py-0.5 shrink-0'>
+            Copy
+          </button>
+          {/* Step 3: Add the refresh password button */}
+          <button
+            onClick={handleRefreshPassword}
+            className='refresh-btn text-center px-3 py-0.5 shrink-0'>
+            <i className="fa-solid fa-arrows-rotate"></i>
+          </button>
+        </div>
+        <div className='options-container flex justify-center text-sm gap-x-2'>
+          <div className='flex item-center gap-x-1'>
+            <input
+              type='range'
+              min={6}
+              max={100}
+              value={state.length}
+              id='length-slider'
+              className='cursor-pointer'
+              onChange={(e) => setState((prev) => ({ ...prev, length: parseInt(e.target.value) }))}
+            />
+            <label htmlFor='length-slider'>Length: {state.length}</label>
+          </div>
+          <div className='flex item-center gap-x-1'>
+            <input
+              type='checkbox'
+              defaultChecked={state.numberAllowed}
+              id='numberInput'
+              className='cursor-pointer'
+              onChange={() => setState((prev) => ({ ...prev, numberAllowed: !prev.numberAllowed }))}
+            />
+            <label htmlFor='numberInput'>Numbers</label>
+          </div>
+          <div className='flex item-center gap-x-1'>
+            <input
+              type='checkbox'
+              defaultChecked={state.charAllowed}
+              id='charInput'
+              className='cursor-pointer'
+              onChange={() => setState((prev) => ({ ...prev, charAllowed: !prev.charAllowed }))}
+            />
+            <label htmlFor='charInput'>Characters</label>
+          </div>
+        </div>
+      </div>
+      {/* </div> */}
+    </>
+  );
+}
+
+export default App;
